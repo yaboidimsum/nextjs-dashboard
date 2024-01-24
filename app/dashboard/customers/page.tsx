334f0bb1b981678250e07644beaa91218fdedec3
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2024-01-24 19:30:41 +0700

    feat: add metadata for each pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer Dashboard',
+};
+
 const Page = () => {
   return (
     <div>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,14 +4,20 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
     invalid_type_error: 'Please select a customer',
   }),
-  amount: z.coerce.number().gt(0, {message: 'Amount must be greater than $0'}),
-  status: z.enum(['pending', 'paid'], {invalid_type_error: 'Please select an invoice status'}),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Amount must be greater than $0' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
@@ -25,6 +31,25 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid Credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
+  }
+}
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
@@ -60,7 +85,11 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string,prevState:State, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
